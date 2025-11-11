46158232ce341a5e3237cc1348a1ffc6770825df
Blagica Bozhinovska <blagica.bozhinovska@skopje.prospective.ch>
2025-11-11 22:21:14 +0100

    Tasks view detail page

--- src/utils/supaQueries.ts
+++ src/utils/supaQueries.ts
@@ -22,3 +22,20 @@ export const projectQuery = (slug: string) => {
     .single()
 }
 export type Project = QueryData<ReturnType<typeof projectQuery>>
+
+export const taskQuery = (id: string) => {
+  return supabase
+    .from('tasks')
+    .select(
+      `*,
+    projects(
+      id,
+      name,
+      slug
+    )`,
+    )
+    .eq('id', parseInt(id))
+    .single()
+}
+
+export type Task = QueryData<ReturnType<typeof taskQuery>>
