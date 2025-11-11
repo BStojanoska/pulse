fb65f93cb05fe054138847ca6691d99cb01a2f8b
Blagica Bozhinovska <blagica.bozhinovska@skopje.prospective.ch>
2025-11-11 18:08:08 +0100

    Move queries to utils

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -3,15 +3,12 @@
 </template>
 
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
 import { usePageStore } from '@/stores/page'
-import type { QueryData } from '@supabase/supabase-js'
+import { tasksWithProjectsQuery, type TasksWithProjects } from '@/utils/supaQueries'
+import type { ColumnDef } from '@tanstack/vue-table'
 
 usePageStore().pageData.title = 'Tasks'
-const tasksWithProjectsQuery = supabase.from('tasks').select(`*, projects(id, name, slug)`)
-type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>
 
 const tasks = ref<TasksWithProjects | null>()
 const getTasks = async () => {
