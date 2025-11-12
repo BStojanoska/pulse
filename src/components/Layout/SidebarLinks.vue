af480ce9fbed44aa3fc2ce7d4673d6581e8c74c6
Blagica Bozhinovska <blagica.bozhinovska@skopje.prospective.ch>
2025-11-12 23:57:59 +0100

    logout

--- src/components/Layout/SidebarLinks.vue
+++ src/components/Layout/SidebarLinks.vue
@@ -1,23 +1,39 @@
 <template>
-  <RouterLink
-    exactActiveClass="text-primary bg-muted"
-    v-for="link in links"
-    :key="link.title"
-    :to="link.to"
-    class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
-  >
-    <iconify-icon :icon="link.icon"></iconify-icon>
-    <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
-  </RouterLink>
+  <template v-for="link in links">
+    <RouterLink
+      v-if="link.to"
+      exactActiveClass="text-primary bg-muted"
+      :key="`${link.title}-link`"
+      :to="link.to"
+      class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
+    >
+      <iconify-icon :icon="link.icon"></iconify-icon>
+      <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+    </RouterLink>
+
+    <div
+      v-else
+      :key="`${link.title}-div`"
+      class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground cursor-pointer"
+      @click="$emit('actionClicked', link.title)"
+    >
+      <iconify-icon :icon="link.icon"></iconify-icon>
+      <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+    </div>
+  </template>
 </template>
 
 <script setup lang="ts">
 interface LinkProp {
   title: string
-  to: string
+  to?: string
   icon: string
 }
 defineProps<{
   links: LinkProp[]
+}>()
+
+defineEmits<{
+  (e: 'actionClicked', title: string): void
 }>()
 </script>
